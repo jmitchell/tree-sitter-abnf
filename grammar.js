84742589822bc72e0c7390bd7af76fb63f81becd
Jacob Mitchell <jmitchell@member.fsf.org>
2018-10-21 13:47:18 -0700

    Initial support for comments and proper rules

--- grammar.js
+++ grammar.js
@@ -5,33 +5,62 @@ module.exports = grammar({
 
   // TODO: revisit conflicts
   conflicts: $ => [
-    [$.__concatenation]
+    [$.__concatenation],
+    [$.c_wsp],
+    [$.elements],
+    [$.__alternation]
   ],
 
   rules: {
-    source_file: $ => seq('start = ', $.__alternation, '\r\n'),
+    source_file: $ => $.rulelist,
 
-    __c_wsp: $ => choice(
-      $.WSP,
-      seq($.__c_nl, $.WSP)
+    rulelist: $ => repeat1(choice(
+      $.rule,
+      seq(repeat($.c_wsp), $.c_nl)
+    )),
+
+    rule: $ => seq($.rulename, $.defined_as, $.elements, $.c_nl),
+
+    rulename: $ => seq(
+      $.ALPHA,
+      repeat(choice($.ALPHA, $.DIGIT, "-"))
     ),
 
-    __c_nl: $ => $.CRLF,
-    
+    defined_as: $ => seq(
+      repeat($.c_wsp),
+      choice("=", "=/"),
+      repeat($.c_wsp)
+    ),
+
+    elements: $ => seq($.__alternation, repeat($.c_wsp)),
+
+    c_wsp: $ => choice(
+      $.WSP,
+      seq($.c_nl, $.WSP)
+    ),
+
+    c_nl: $ => choice($.comment, $.CRLF),
+
+    comment: $ => seq(
+      ";",
+      repeat(choice($.WSP, $.VCHAR)),
+      $.CRLF
+    ),
+
     __alternation: $ => seq(
       $.__concatenation,
       repeat(seq(
-        $.__c_wsp,
+        $.c_wsp,
         "/",
-        $.__c_wsp,
+        $.c_wsp,
         $.__concatenation
       ))
     ),
-    
+
     __concatenation: $ => seq(
       $.core_rulename,
       repeat(seq(
-        repeat1($.__c_wsp),
+        repeat1($.c_wsp),
         $.core_rulename
       ))
     ),
@@ -55,15 +84,21 @@ module.exports = grammar({
       "WSP"
     ),
 
+    ALPHA: $ => /[A-Za-z]/,
+
+    DIGIT: $ => /[0-9]/,
+
     CR: $ => "\r",
 
     CRLF: $ => seq($.CR, $.LF),
-    
+
     HTAB: $ => "\t",
 
     LF: $ => "\n",
-    
+
     SP: $ => " ",
+
+    VCHAR: $ => /[\x21-\x7E]/,
 
     WSP: $ => choice($.SP, $.HTAB)
   }
