e2d2d3d461f7e35dcec229a8f0d642233c194c02
Jacob Mitchell <jmitchell@member.fsf.org>
2018-10-21 13:06:33 -0700

    Make whitespace explicit

--- grammar.js
+++ grammar.js
@@ -1,10 +1,18 @@
 module.exports = grammar({
   name: 'abnf',
 
-  rules: {
-    source_file: $ => seq('start = ', $.__concatenation),
+  extras: $ => [],
 
-    __concatenation: $ => repeat1($.core_rulename),
+  rules: {
+    source_file: $ => seq('start = ', $.__concatenation, '\r\n'),
+
+    __concatenation: $ => seq(
+      $.core_rulename,
+      repeat(seq(
+        repeat1(' '),
+        $.core_rulename
+      ))
+    ),
 
     core_rulename: $ => choice(
       "ALPHA",
